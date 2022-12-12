e6df449b7e7707085a33ea737aef307fbcbad3aa
Kang Hyojun <iam.kanghyojun@gmail.com>
2022-12-12 16:13:52 +0900

    indentation & import,export bug

--- src/template.ts
+++ src/template.ts
@@ -1,5 +1,5 @@
 export const indexTs = (componentName: string): string => {
-  return `import { default } from './${componentName}';`;
+  return `export { default, type ${componentName}Props } from './${componentName}';`;
 };
 
 export const componentTs = (componentName: string): string => {
@@ -20,20 +20,20 @@ export default memo(${componentName});
 export const componentStoriesTs = (componentName: string): string => {
   return `import { Meta, Story } from '@storybook/react/types-6-0';
 
-  import ${componentName}, { ${componentName}Props } from './${componentName}';
-  
-  export default {
-    title: 'components/${componentName}',
-    component: ${componentName},
-  } as Meta;
-  
-  const Template: Story<${componentName}Props> = (args) => {
-    return <${componentName} {...args} />;
-  };
-  
-  export const Plain = Template.bind({});
-  Plain.args = {};
-  `;
+import ${componentName}, { ${componentName}Props } from './${componentName}';
+
+export default {
+  title: 'components/${componentName}',
+  component: ${componentName},
+} as Meta;
+
+const Template: Story<${componentName}Props> = (args) => {
+  return <${componentName} {...args} />;
+};
+
+export const Plain = Template.bind({});
+Plain.args = {};
+`;
 };
 
 export const componentTestTs = (componentName: string): string => {
